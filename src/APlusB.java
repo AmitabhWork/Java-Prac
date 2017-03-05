68e118b07fe2e5a44e3265aa16406e7a8c1dc9c0
Amitabhs <Amitabhs@mac.com>
2017-03-05 14:50:18 +0530

    deleted Aplus b file and changed missingnumber file

--- src/APlusB.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-class APlusB {
-   public static void main(String [] ag){
-       Test ab =new Test();
-   }
-}
--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -19,10 +19,10 @@ public class MissingNumber {
                 arr[i] = sc.nextInt();
             }
             //numbers he pickuped form the floor
-//            int x = sc.nextInt();
-
-//            System.out.println(search(arr, n, x));
-//            t--;
+            int x = sc.nextInt();
+            System.out.println(search(arr, n, x));
+            //checked by amitabh
+            t--;
         }
     }
 
